459af618ed7a4d72521d295ff42eef82eab5e299
AzureAerial <jake@azureaerial.io>
2024-10-03 13:39:31 -0400

    Fix manifest and paths

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "558914520"
+    serviceWorkerVersion: "3572960496"
   }
 });
